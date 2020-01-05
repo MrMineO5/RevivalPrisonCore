b2844cba35b2174f613b2e96babe22d947585df4
BuildTools <unconfigured@null.spigotmc.org>
2020-01-05 12:10:49 +0100

    Fixed Socket Gems in lore & negative lucky block rewards

--- src/main/java/net/ultradev/prisoncore/pickaxe/LuckyBlock.java
+++ src/main/java/net/ultradev/prisoncore/pickaxe/LuckyBlock.java
@@ -1,7 +1,3 @@
-/*
- * Copyright (c) 2019. UltraDev
- */
-
 package net.ultradev.prisoncore.pickaxe;
 
 import net.ultradev.prisoncore.playerdata.PlayerData;
@@ -16,98 +12,98 @@ import org.bukkit.entity.Player;
 import java.util.Random;
 
 public class LuckyBlock {
-    public static RewardApplicator getLuckBlockReward(int favoredLevel, boolean message, double favoredSocket) {
-        boolean more = MathUtils.isRandom(favoredSocket, 100.0);
-        double i = MathUtils.random(0.0, 100.0);
-        Random rand = new Random();
+    public static RewardApplicator getLuckBlockReward(final int favoredLevel, final boolean message, final double favoredSocket) {
+        final boolean more = MathUtils.isRandom(favoredSocket, 100.0);
+        final double i = MathUtils.random(0.0, 100.0);
+        final Random rand = new Random();
         if (i < 60.0) {
             long tokenCount = (favoredLevel + 1) * 5;
             if (more) {
                 tokenCount = (long) Math.floor(tokenCount * MathUtils.random(1.5, 2.0));
             }
-            return new TokenReward(tokenCount).getApplicator(message ? (name, amount) -> "§eLucky Block » §7You found §e" + amount + " Tokens§7!" : null);
+            return new TokenReward(tokenCount).getApplicator(message ? ((name, amount) -> "§eLucky Block » §7You found §e" + amount + " Tokens§7!") : null);
         }
         if (i < 95.0) {
             String key = "mine";
-            int amount = Math.floorDiv(favoredLevel, 200) + 1;
-            double r = MathUtils.random(0.0, 100.0);
+            int amount2 = Math.floorDiv(favoredLevel, 200) + 1;
+            final double r = MathUtils.random(0.0, 100.0);
             if (favoredLevel >= 500) {
                 key = "legendary";
             }
-            if (r <= 85) {
+            if (r <= 85.0) {
                 if (favoredLevel >= 100) {
                     key = "rare";
                 }
-                amount += 2;
+                amount2 += 2;
             }
             if (r <= 65.0) {
                 key = "mine";
-                amount += 1;
+                ++amount2;
             }
             if (more) {
-                amount = (int) Math.floor(amount * MathUtils.random(1.3, 1.7));
+                amount2 = (int) Math.floor(amount2 * MathUtils.random(1.3, 1.7));
             }
-            return new KeyReward(key, amount).getApplicator(message ? (name, am) -> "§eLucky Block » §7You found " + am + "x §e" + name + "§7!" : null);
+            return new KeyReward(key, amount2).getApplicator(message ? ((name, am) -> "§eLucky Block » §7You found " + am + "x §e" + name + "§7!") : null);
         }
         int dust = Math.floorDiv(favoredLevel, 200) + 2;
         if (more) {
             dust = (int) Math.floor(dust * MathUtils.random(1.3, 1.7));
         }
-        return new SocketGemDustReward(dust).getApplicator(message ? (name, am) -> "§eLucky Block» §7You found " + am + " Socket Gem Dust" : null);
+        return new SocketGemDustReward(dust).getApplicator(message ? ((name, am) -> "§eLucky Block» §7You found " + am + " Socket Gem Dust") : null);
     }
 
-    public static void runLuckBlockReward(Player player, int favoredLevel, double favoredSocket) {
+    public static void runLuckBlockReward(final Player player, final int favoredLevel, final double favoredSocket) {
         getLuckBlockReward(favoredLevel, PlayerData.getSetting(player, "lb_message"), favoredSocket).applyReward(player);
     }
 
-    public static RewardApplicator getSuperLuckBlockReward(int favoredLevel, boolean message, double favoredSocket) {
-        boolean more = MathUtils.isRandom(favoredSocket, 100.0);
-        double i = MathUtils.random(0.0, 100.0);
-        Random rand = new Random();
+    public static RewardApplicator getSuperLuckBlockReward(final int favoredLevel, final boolean message, final double favoredSocket) {
+        final boolean more = MathUtils.isRandom(favoredSocket, 100.0);
+        final double i = MathUtils.random(0.0, 100.0);
+        final Random rand = new Random();
         if (i < 60.0) {
             long tokenCount = favoredLevel * 5;
             if (more) {
                 tokenCount = (long) Math.floor(tokenCount * MathUtils.random(1.5, 2.0));
             }
-            return new TokenReward(tokenCount).getApplicator(message ? (name, amount) -> "§cSuper Lucky Block » §7You found §e" + amount + " Tokens§7!" : null);
+            return new TokenReward(tokenCount).getApplicator(message ? ((name, amount) -> "§cSuper Lucky Block » §7You found §e" + amount + " Tokens§7!") : null);
         }
         if (i < 90.0) {
             String key = "mine";
-            int amount = Math.floorDiv(favoredLevel, 200) - 1;
-            double r = MathUtils.random(0.0, 100.0);
+            int amount2 = Math.floorDiv(favoredLevel, 200) - 1;
+            final double r = MathUtils.random(0.0, 100.0);
             if (favoredLevel >= 500) {
                 key = "legendary";
             }
-            if (r <= 85) {
+            if (r <= 85.0) {
                 if (favoredLevel >= 100) {
                     key = "rare";
                 }
-                amount += 2;
+                amount2 += 2;
             }
             if (r <= 65.0) {
                 key = "mine";
-                amount += 1;
+                ++amount2;
             }
             if (more) {
-                amount = (int) Math.floor(amount * MathUtils.random(1.3, 1.7));
+                amount2 = (int) Math.floor(amount2 * MathUtils.random(1.3, 1.7));
             }
-            return new KeyReward(key, amount).getApplicator(message ? (name, am) -> "§cSuper Lucky Block » §7You found " + am + "x §e" + name + "§7!" : null);
+            return new KeyReward(key, amount2).getApplicator(message ? ((name, am) -> "§cSuper Lucky Block » §7You found " + am + "x §e" + name + "§7!") : null);
         }
         if (i < 95.0) {
             int dust = Math.floorDiv(favoredLevel, 100) + 5;
             if (more) {
                 dust = (int) Math.floor(dust * MathUtils.random(1.3, 1.7));
             }
-            return new SocketGemDustReward(dust).getApplicator(message ? (name, am) -> "§cSuper Lucky Block» §7You found " + am + " Socket Gem Dust" : null);
+            return new SocketGemDustReward(dust).getApplicator(message ? ((name, am) -> "§cSuper Lucky Block» §7You found " + am + " Socket Gem Dust") : null);
         }
         int platinumTokens = Math.floorDiv(favoredLevel, 50) + 10;
         if (more) {
             platinumTokens = (int) Math.floor(platinumTokens * MathUtils.random(1.5, 2.0));
         }
-        return new PlatinumTokenReward(platinumTokens).getApplicator(message ? (name, amount) -> "§cSuper Lucky Block » §7You found §b" + amount + " Platinum Tokens§7!" : null);
+        return new PlatinumTokenReward(platinumTokens).getApplicator(message ? ((name, amount) -> "§cSuper Lucky Block » §7You found §b" + amount + " Platinum Tokens§7!") : null);
     }
 
-    public static void runSuperLuckBlockReward(Player player, int favoredLevel, double luckySocket) {
+    public static void runSuperLuckBlockReward(final Player player, final int favoredLevel, final double luckySocket) {
         getSuperLuckBlockReward(favoredLevel, PlayerData.getSetting(player, "lb_message"), luckySocket).applyReward(player);
     }
 }
--- src/main/java/net/ultradev/prisoncore/pickaxe/PickaxeLore.java
+++ src/main/java/net/ultradev/prisoncore/pickaxe/PickaxeLore.java
@@ -1,7 +1,3 @@
-/*
- * Copyright (c) 2019. UltraDev
- */
-
 package net.ultradev.prisoncore.pickaxe;
 
 import net.ultradev.prisoncore.pickaxe.socketgems.SocketGem;
@@ -14,30 +10,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PickaxeLore {
-    public static double getPercent(BigInteger xp, BigInteger maxxp) {
-        return xp.multiply(BigInteger.valueOf(1000)).divide(maxxp).doubleValue() / 10.0;
+    public static double getPercent(final BigInteger xp, final BigInteger maxxp) {
+        return xp.multiply(BigInteger.valueOf(1000L)).divide(maxxp).doubleValue() / 10.0;
     }
 
-    public static List<String> generateLevelLines(BigInteger xp, int level) {
-        BigInteger maxxp = getXpRequired(level);
-        List<String> lines = new ArrayList<>();
+    public static List<String> generateLevelLines(final BigInteger xp, final int level) {
+        final BigInteger maxxp = getXpRequired(level);
+        final List<String> lines = new ArrayList<String>();
         lines.add("§b§lLevel " + level);
         lines.add("§8[" + TextUtils.generateBar('a', '7', 50, xp, maxxp) + "§8] §f" + getPercent(xp, maxxp) + "§7%");
         lines.add("§7(§f" + NumberUtils.formatFull(xp) + "§7 / " + NumberUtils.formatFull(maxxp) + "§7)");
         return lines;
     }
 
-    public static BigInteger getXpRequired(int level) {
-        return BigInteger.valueOf(1000).multiply(BigDecimal.valueOf(1.3).pow(level).toBigInteger());
+    public static BigInteger getXpRequired(final int level) {
+        return BigInteger.valueOf(1000L).multiply(BigDecimal.valueOf(1.3).pow(level).toBigInteger());
     }
 
-    static List<String> generateSocketLines(List<SocketGem> gems) {
-        List<String> lines = new ArrayList<>();
-        for (SocketGem sg : gems) {
+    static List<String> generateSocketLines(final List<SocketGem> gems) {
+        final List<String> lines = new ArrayList<>();
+        for (final SocketGem sg : gems) {
             if (sg == null) {
                 lines.add("§a<Empty Slot>");
             } else {
-                lines.add(sg.getItem().getItemMeta().getDisplayName());
+                lines.add(sg.getItem().getItemMeta().getDisplayName() + " (" + sg.getPercent() + ")");
             }
         }
         return lines;
